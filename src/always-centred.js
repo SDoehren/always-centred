1059084a0ebe2adb6374f27d89454dda37e7b2e2
SDoehren <samdoehren@gmail.com>
2021-01-03 14:41:51 +0000

    v0.0.41

--- src/always-centred.js
+++ src/always-centred.js
@@ -1,9 +1,14 @@
+// Import JavaScript modules.
+
+import registerSettings from './module/settings';
+
+
 /* ------------------------------------ */
 /* Initialize module                    */
 /* ------------------------------------ */
 Hooks.once('init', async () => {
     console.log('always-centred | Initializing always-centred');
-
+    registerSettings();
 
 });
 
