02c2ab7ce1a2a9d57a94952bbd27a580561b8eae
SDoehren <samdoehren@gmail.com>
2021-01-03 14:34:42 +0000

    v0.0.4

--- src/always-centred.js
+++ src/always-centred.js
@@ -3,21 +3,7 @@
 /* ------------------------------------ */
 Hooks.once('init', async () => {
     console.log('always-centred | Initializing always-centred');
-    game.settings.register("always-centred", "alwayscenter", {
-        name: "Centre View Continuously?",
-        scope: "client",
-        config: true,
-        default: false,
-        type: Boolean
-    });
 
-    game.settings.register("always-centred", "maxzoom", {
-        name: "max zoom level?",
-        scope: "client",
-        config: true,
-        default: 1,
-        type: Number
-    });
 
 });
 
---
+++ src/module/constants.js
@@ -0,0 +1,13 @@
+/**
+ * A class storing our module's constants.
+ */
+export default class Constants {
+    /**
+     * The name and identifier of the module.
+     *
+     * @return {string}
+     */
+    static get MODULE_NAME() {
+        return 'always-centred';
+    }
+}
